31e69177509d2be041e750322a78f29fe4c885c4
Marco Stevanon <marco.stevanon@gmail.com>
2019-06-20 12:21:01 +0200

    add tests

---
+++ test/test-telnet.js
@@ -0,0 +1,28 @@
+var Telnet = require('telnet-client')
+var connection = new Telnet()
+
+var params = {
+    host: '192.168.100.2',
+    port: 7171,
+    passwordPrompt: 'Enter password:',
+    password: 'adept',
+    shellPrompt: '',
+    timeout: 3000,
+    // removeEcho: 4
+}
+
+connection.connect(params)
+    .then(
+        (prompt) => {
+            console.log(prompt);
+            connection.exec('status')
+                .then(console.log)
+                .catch(console.log)
+        },
+        (error) => {
+            console.log('promises reject:', error)
+        })
+    .catch(function (error) {
+        console.log(error);
+        // handle the throw (timeout)
+    })
